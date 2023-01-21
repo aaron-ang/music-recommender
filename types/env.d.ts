ac52d81e9e4b5340aa00dc541ba777a5eff9158a
aaron-ang <67321817+aaron-ang@users.noreply.github.com>
2023-01-20 21:08:56 -0500

    first commit

---
+++ types/env.d.ts
@@ -0,0 +1,6 @@
+declare module "@env" {
+  export const SHAZAM_API_KEY: string;
+  export const SHAZAM_API_HOST: string;
+  export const SPOTIFY_CLIENT_ID: string;
+  export const SPOTIFY_CLIENT_SECRET: string;
+}
